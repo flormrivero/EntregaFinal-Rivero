cbf199749c3e8fdb5eef0137b26440beaf4c512a
Florencia <flormrivero@gmail.com>
2023-03-30 11:24:34 -0300

    NavBar

---
+++ PreEntrega1Rivero/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>Ecommerce</h3>
+            <div>
+                <button>Barritas de Cereal</button>
+                <button>Desayunos saludables</button>
+                <button>Empresas</button>
+            </div>
+        </nav>
+    )
+}
+export default NavBar
