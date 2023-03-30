7e55304b19f9b7e9b8eb24f75a057d0604aaa1fd
Florencia <flormrivero@gmail.com>
2023-03-30 11:39:35 -0300

    cart

--- PreEntrega1Rivero/src/components/NavBar/NavBar.jsx
+++ PreEntrega1Rivero/src/components/NavBar/NavBar.jsx
@@ -1,3 +1,5 @@
+import CartWidget from "../CartWidget/CartWidget"
+
 const NavBar = () => {
     return (
         <nav>
@@ -7,6 +9,7 @@ const NavBar = () => {
                 <button>Desayunos saludables</button>
                 <button>Empresas</button>
             </div>
+            <CartWidget />
         </nav>
     )
 }
