c51ff2bf629374f7a98946281205bad4add75de4
Florencia <flormrivero@gmail.com>
2023-04-04 18:28:45 -0300

    bootstrap

--- PreEntrega1Rivero/src/App.jsx
+++ PreEntrega1Rivero/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
--- PreEntrega1Rivero/src/components/NavBar/NavBar.jsx
+++ PreEntrega1Rivero/src/components/NavBar/NavBar.jsx
@@ -1,16 +1,29 @@
 import CartWidget from "../CartWidget/CartWidget"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 const NavBar = () => {
-    return (
-        <nav>
-            <h3>Ecommerce</h3>
-            <div>
-                <button>Barritas de Cereal</button>
-                <button>Desayunos saludables</button>
-                <button>Empresas</button>
-            </div>
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">E-Commerce</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#barritas">Barritas de Cereal</Nav.Link>
+            <Nav.Link href="#desayunos">Desayunos saludables</Nav.Link>
+            </Nav>
+          <Nav>
+            <Nav.Link eventKey={2} href="#cart">
             <CartWidget />
-        </nav>
-    )
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
-export default NavBar
+
+export default NavBar;
