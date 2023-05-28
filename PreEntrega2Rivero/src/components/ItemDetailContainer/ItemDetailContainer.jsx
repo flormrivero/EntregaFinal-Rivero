8e6bbcbd1bad221b00f154b856cc472b6428bb45
Florencia <flormrivero@gmail.com>
2023-05-28 15:55:55 -0300

    itemDetail

--- PreEntrega2Rivero/src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ PreEntrega2Rivero/src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,43 +1,28 @@
 import { useState, useEffect } from "react"
 import ItemDetail from '../ItemDetail/ItemDetail'
 import {useParams} from 'react-router-dom'
-
 import { getDoc, doc } from 'firebase/firestore'
 import { db } from '../../services/firebase/firebaseConfig'
 
-import { Loading } from "../Loading/Loading"
-
 const ItemDetailContainer = () => {
-    const [product, setProduct] = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
-
-    const { itemId } = useParams ()
-
+    const [product, setProduct] = useState([])
+    const { itemId } = useParams ();
+    const getItem = async () => {
+        try{
+            const result = await getDoc(doc(db, '39660', itemId))
+            setProduct(result.data())
+        }catch(error){
+            console.log(error);
+        }
+    }
     useEffect(() => {
-        setIsLoading(true)
-
-        const docRef = doc(db, '39660', itemId)
-
-        getDoc(docRef)
-            .then(response => {
-                const data = response.data()
-                const productsAdapted = { id: response.id, ...data}
-                setProduct(productsAdapted)
-        })
-            .catch(error => {
-                console.log(error)
-        })
-        .finally(()=> setIsLoading(false))
-    }, [itemId])
+        getItem();
+    }, [product]);
 
     return (
-        <>
-            {isLoading ? 
-                    <Loading />
-                :
-                    <ItemDetail product={product} />
-            }
-        </>
+        <React.Fragment>
+            <ItemDetail item={product} id={itemId}></ItemDetail>
+        </React.Fragment>
         
     ) 
 }
