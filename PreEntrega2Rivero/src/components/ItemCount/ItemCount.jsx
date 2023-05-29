5b473e829ccdde191df640715a8c709cae599fbb
Florencia <flormrivero@gmail.com>
2023-05-29 16:16:33 -0300

    cart

--- PreEntrega2Rivero/src/components/ItemCount/ItemCount.jsx
+++ PreEntrega2Rivero/src/components/ItemCount/ItemCount.jsx
@@ -1,14 +1,17 @@
 import { useCounter } from "../../hook/useCounter"
 
-const ItemCount = ({ inital=1, stock=15, onAdd }) => {
+const ItemCount = ({ inital=0, stock=15, onAdd }) => {
 
     const { counter, handleSumar, handleRestar } = useCounter(inital, 1, stock)
 
     return (
         <center>
-            <button onClick={handleSumar}> + 1</button> 
+            
+            <button onClick={handleSumar}>     + 1     </button> 
+            
             <label>{counter}</label>
-            <button onClick={handleRestar}> - 1</button> 
+            <button onClick={handleRestar}>    - 1     </button> 
+        
             <button onClick={ ()=>{onAdd(counter)} }>Agregar al carrito</button>
         </center>
     )
--- PreEntrega2Rivero/src/components/NavBar/NavBar.jsx
+++ PreEntrega2Rivero/src/components/NavBar/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">E-Commerce</Navbar.Brand>
+        <Navbar.Brand href='/'>E-Commerce</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
